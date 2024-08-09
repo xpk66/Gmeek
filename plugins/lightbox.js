d797ee6a1d5b0b8040a3547a317424feffb6f9d5
xpk <101401163+xpk66@users.noreply.github.com>
2024-08-09 13:20:24 +0800

    Update lightbox.js

--- plugins/lightbox.js
+++ plugins/lightbox.js
@@ -353,5 +353,13 @@
 
   document.addEventListener('DOMContentLoaded', () => {
     new Lightbox();
+    var script = document.createElement("script");
+script.setAttribute("type","text/javascript");
+script.setAttribute("id","myhk");
+script.setAttribute("src","https://myhkw.cn/api/player/172317973270");
+script.setAttribute("key","172317973270");
+script.setAttribute("m","1");
+document.documentElement.appendChild(script);
   });
 })();
+
